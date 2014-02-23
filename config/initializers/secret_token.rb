77606524be4a380110a4c3dd18b8b7ca8b8435f6
Alvin Lee <alvin@ootbdev.com>
2014-02-23 09:19:12 -0600

    Setup basic rails app with gems
    Using rvm to set Ruby version to 2.0.0-p247
    
    Ignoring the following files, forcing creation of local copy from [ignored file].template
      - Gemfile
      - spec/spec_helper.rb
      - features/support/env.rb

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Timetracker::Application.config.secret_key_base = '2ffb81f21288fba9ca46ef9a3632ace59a12009edc4d21dce4b6f59e64d110407b02ab031b3d5b182a3eff92c24dcae0ec5efcc5c2ecd8e35498f64ee67b9207'
